53725db4250c0e7e968b63f9276de88ff703e5ae
David Salazar <dssalaza@gmail.com>
2025-10-08 20:24:51 -0700

    feat: create OS specific wdio config files

---
+++ config/wdio.android.conf.js
@@ -0,0 +1,43 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const config = {
+    specs: [
+        '../tests/specs/*.e2e.js'
+    ],
+    maxInstances: 1,
+    maxInstancesPerCapability: 1,
+    logLevel: 'info',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    port: 4725,
+    services: [
+        ['appium', {
+            isHybridApp: true,
+            command: 'appium',
+            args: {
+                port: 4725,
+                relaxedSecurity: true
+            }
+        }]
+    ],
+    framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 65 * 1000
+    },
+    // iOS specific capabilities
+    capabilities: [{
+        platformName: 'Android',
+        'appium:automationName': 'UiAutomator2',
+        'appium:app': path.resolve(__dirname, '../apps/android-app.apk'),
+        'appium:noReset': false,
+        'appium:fullReset': true,
+        'appium:dontStopAppOnReset': true,
+        'appium:newCommandTimeout': 240
+    }],
+}; 
