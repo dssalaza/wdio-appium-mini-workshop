35f02391fb34e66cc98760ed1ee3c0fb1c446193
David Salazar <dssalaza@gmail.com>
2025-10-14 14:05:15 -0700

    feat: create login and native alert test

--- tests/specs/login.e2e.js
+++ tests/specs/login.e2e.js
@@ -1,6 +1,47 @@
 describe('App Test', () => {
-    it('Should launch the app successfully', async () => {
-        // Wait for the app to load
-        await driver.pause(3000); 
+    it.skip('Should login successfully', async () => {
+        // Navigate to the login screen
+        await $('~Login').click();
+        await $('~Login-screen').waitForDisplayed(true);
+
+        // Enter credentials
+        await $('~input-email').setValue('test@webdriver.io');
+        await $('~input-password').setValue('Test1234!');
+
+        // Click on the login button
+        await $('~button-LOGIN').click();
+
+    });
+
+});
+
+describe('Should show a native alert when login is successfull', () => {
+
+    // XPath selector
+    const ANDROID_ALERT_TITLE = '*//android.widget.TextView[@resource-id="android:id/alertTitle"]'
+
+    // IOS Predicate String selector it uses XCUITest
+    const IOS_ALERT = '-ios predicate string:type == \'XCUIElementTypeAlert\''
+
+    it('Should login successfully and accept the native alert', async () => {
+
+        // Navigate to the login screen
+        await $('~Login').click();
+        await $('~Login-screen').waitForDisplayed();
+
+        // Enter credentials
+        await $('~input-email').setValue('test@webdriver.io');
+        await $('~input-password').setValue('Test1234!');
+
+        // Click on the login button
+        await $('~button-LOGIN').click();
+
+        // Wait for the native alert to appear using the driver object
+        if (await driver.isIOS) {
+            expect(await $(IOS_ALERT).waitForDisplayed()).toBe(true);
+        } else {
+            expect(await $(ANDROID_ALERT_TITLE).waitForDisplayed()).toBe(true);
+        }    
+
     });
 });
