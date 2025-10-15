0e66b9dbbc77bc68e2752fad8e95112e9fcba867
David Salazar <dssalaza@gmail.com>
2025-10-14 19:28:30 -0700

    feat: added describe block to test

--- tests/specs/swipe.e2e.js
+++ tests/specs/swipe.e2e.js
@@ -1,21 +1,25 @@
-it('should be able to swipe vertical by finding the surprise', async () => {
-    const SwipeScreen = $('~Swipe-screen');
-    const WDIOLogo = $('~WebdriverIO logo');
-    const SwipeTab = $('~Swipe');
+describe('Swipe test: ', () => {
 
-    // Wait for the Swipe tab to be displayed
-    await SwipeTab.waitForDisplayed(true);
-    await SwipeTab.click();
-    await SwipeScreen.waitForDisplayed(true);
+    it('Should be able to swipe vertical by finding the surprise', async () => {
 
-    // Scroll up to find the WDIOLogo
-    await WDIOLogo.scrollIntoView({
-        scrollableElement: await SwipeScreen,
-        direction: 'up',
-        maxScrolls: 5,
-        percent: 0.99,
+        const SwipeScreen = $('~Swipe-screen');
+        const WDIOLogo = $('~WebdriverIO logo');
+        const SwipeTab = $('~Swipe');
+
+        // Wait for the Swipe tab to be displayed
+        await SwipeTab.waitForDisplayed(true);
+        await SwipeTab.click();
+        await SwipeScreen.waitForDisplayed(true);
+
+        // Scroll up to find the WDIOLogo
+        await WDIOLogo.scrollIntoView({
+            scrollableElement: await SwipeScreen,
+            direction: 'up',
+            maxScrolls: 5,
+            percent: 0.99,
+        });
+
+        // Wait for the WDIOLogo to be displayed
+        await expect(WDIOLogo).toBeDisplayed();
     });
-
-    // Wait for the WDIOLogo to be displayed
-    await expect(WDIOLogo).toBeDisplayed();
 });
